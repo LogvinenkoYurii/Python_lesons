a8369cd754e4fb5f80f410b8c8d2b8256f0bfbf0
Yura <yura.logvinenko.96@gmail.com>
2021-11-30 22:39:15 +0200

    HomeWork4

---
+++ HomeWork4.py
@@ -0,0 +1,32 @@
+# a = input("введите текст: ")
+# for b in range(len(a)-1,-1,-1):
+#     print(a[b], end="")
+
+
+
+# a = int(input("Введите число: " ))
+#
+# for i in range(1, a+1):
+#     print("*"*i)
+# for n in range(a-1,-1,-1):
+#     print("*"*n)
+
+
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# if a < b:
+#     for num1 in range(a, b+1):
+#         print(num1)
+# elif a==b:
+#     print("Числа равны!")
+# else:
+#     for num2 in range(b, a+1):
+#         print(num2)
+
+
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# for c in range(a,b+1):
+#     print(c)
