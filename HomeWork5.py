95b29bbe7423ceacf96995e5c8ec1a3f8d5c7bba
Yura <yura.logvinenko.96@gmail.com>
2021-12-09 21:32:40 +0200

    HomeWork5

---
+++ HomeWork5.py
@@ -0,0 +1,64 @@
+import random
+
+
+# l1 = []
+# l2 = []
+# n = int(input("Введите количество значений в списке: "))
+# for _ in range(n):
+#     b = int(input("Введите число в список: "))
+#     l1.append(b)
+# print(l1)
+#
+# for i in l1:
+#     if l2.count(i) == 0:
+#         l2.append(i)
+# print(l2)
+#
+# l3 = list(set(l1))
+# print(l3)
+
+
+# l1 = []
+# l2 = []
+# n = int(input("Введите количество значений в списке: "))
+# for _ in range(n):
+#     b = int(input("Введите число в список: "))
+#     l1.append(b)
+# print(l1)
+#
+# for i in l1:
+#     l2.append(i)
+# print(l2)
+
+
+# l1 = []
+# l2 = []
+# l3 = []
+# for _ in range(10):
+#     l1.append(random.randint(0, 10))
+# for _ in range(10):
+#     l2.append(random.randint(0, 10))
+# print(l1)
+# print(l2)
+# for i in l1:
+#     if l2.count(i) == 0:
+#         l3.append(i)
+# for i in l2:
+#     if l1.count(i) == 0:
+#         l3.append(i)
+# print(l3)
+
+
+# dict1 = {1: 10, 2: 20}
+# dict2 = {3: 30, 4: 40}
+# dict3 = {5: 50, 6: 60}
+# dict4 = {}
+# for i in (dict1, dict2, dict3):
+#     dict4.update(i)
+# print(dict4)
+
+
+# dict1 = {}
+# for i in range(1, 16):
+#     dict1[i] = i**2
+# print(dict1)
