a45e49e95f5e2966319a9fc885438c8c30b7e73a
Yura <yura.logvinenko.96@gmail.com>
2021-12-26 13:03:31 +0200

    HomeWork8

--- HomeWork8.py
+++ HomeWork8.py
@@ -2,75 +2,75 @@ import random
 
 
 #Задание 1
-# def print_random_num():
-#     num = random.randint(1, 100)
-#     print(num)
-# print_random_num()
+def print_random_num():
+    num = random.randint(1, 100)
+    print(num)
+print_random_num()
 
 
 #Задание 2
-# def get_random_num():
-#     num = random.randint(1, 100)
-#     return (num)
-# result = get_random_num()
-# print(result)
+def get_random_num():
+    num = random.randint(1, 100)
+    return (num)
+result = get_random_num()
+print(result)
 
 
 #Задание 3
-# def print_choice():
-#     num = random.randint(1, 100)
-#     if num > 50:
-#         print("Good")
-#     else:
-#         print("Bad")
-# print_choice()
+def print_choice():
+    num = random.randint(1, 100)
+    if num > 50:
+        print("Good")
+    else:
+        print("Bad")
+print_choice()
 
 
 #Задание 4
-# def upper_string(some_string):
-#     print(some_string.upper())
-# str1 = "my name"
-# upper_string(str1)
+def upper_string(some_string):
+    print(some_string.upper())
+str1 = "my name"
+upper_string(str1)
 
 
 #Задание 5
-# def get_upper_string(some_string):
-#     return some_string.upper()
-#
-# str1 = "my name"
-# value = get_upper_string(str1)
-# print(value)
+def get_upper_string(some_string):
+    return some_string.upper()
+
+str1 = "my name"
+value = get_upper_string(str1)
+print(value)
 
 
 #Задание 6
-# def get_upper_list(list1):
-#     resault_list = list()
-#     for resault in list1:
-#         resault_list.append(resault.upper())
-#     return resault_list
-#
-# value_list = ["qwerty", "asdfgh"]
-# rasult = get_upper_list(value_list)
-# print(rasult)
+def get_upper_list(list1):
+    resault_list = list()
+    for resault in list1:
+        resault_list.append(resault.upper())
+    return resault_list
+
+value_list = ["qwerty", "asdfgh"]
+rasult = get_upper_list(value_list)
+print(rasult)
 
 
 #Задание 7
-# def get_number_list(num):
-#     result_list = list()
-#     for num in range(1, num+1):
-#         result_list.append(num)
-#     return result_list
-# number = 47
-# result = get_number_list(number)
-# print(result)
+def get_number_list(num):
+    result_list = list()
+    for num in range(1, num+1):
+        result_list.append(num)
+    return result_list
+number = 47
+result = get_number_list(number)
+print(result)
 
 
 #Задание 8
-# def get_square_number_list(num):
-#     result_list = list()
-#     for num in range(1, num+1):
-#         result_list.append(num**2)
-#     return result_list
-# number = 47
-# result = get_square_number_list(number)
-# print(result)
+def get_square_number_list(num):
+    result_list = list()
+    for num in range(1, num+1):
+        result_list.append(num**2)
+    return result_list
+number = 47
+result = get_square_number_list(number)
+print(result)
